dd045166150ded959603247a68a7e384052b78b0
한준 <s22073@gsm.hs.kr>
2023-06-27 20:35:54 +0900

    기본 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const URL = `http://ec2-15-164-170-26.ap-northeast-2.compute.amazonaws.com:9040/email`;
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const email = e.target.email.value;
+    axios
+      .post(URL, { email }, { withCredentials: true })
+      .then((res) => {
+        console.log(res.data);
+        toast.success("이메일 확인해줘");
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("에러남");
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={onSubmitHandler}>
+        <input name="email" type="email" placeholder="이메일" />
+        <input type="submit" value="제출" />
+      </form>
+      <ToastContainer />
     </div>
   );
 }
