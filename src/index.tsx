c5b29b5d9df36a70c0101c79973de5b747184788
우엥 <105274015+mini-aron@users.noreply.github.com>
2023-06-29 21:03:59 +0900

    rootreducer설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore as createStore } from 'redux';
+import rootReducer from './stores';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
